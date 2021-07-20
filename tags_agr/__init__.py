dae20932a96886bf88c6e3d0f477a5b655d67292
vcslav-v <vcslav.v@gmail.com>
2021-07-20 13:56:33 +0300

    fix

--- tags_agr/__init__.py
+++ tags_agr/__init__.py
@@ -1,4 +1,15 @@
 """Main module tags_agr project."""
+import os
+
 UPLOAD_FOLDER_ADOBE = 'adobe_upload'
 UPLOAD_FOLDER_RESIZE = 'resize_upload'
 RESULT_FOLDER_RESIZE = 'resize_result'
+
+if not os.path.isdir(UPLOAD_FOLDER_ADOBE):
+    os.mkdir(UPLOAD_FOLDER_RESIZE)
+
+if not os.path.isdir(UPLOAD_FOLDER_RESIZE):
+    os.mkdir(UPLOAD_FOLDER_RESIZE)
+
+if not os.path.isdir(RESULT_FOLDER_RESIZE):
+    os.mkdir(RESULT_FOLDER_RESIZE)
