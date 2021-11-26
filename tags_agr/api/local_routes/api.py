96795dd27650455e321491436b75d1b1dc31b7da
Vcslav <vcslav.v@gmail.com>
2021-11-26 10:33:13 +0300

    add token

--- tags_agr/api/local_routes/api.py
+++ tags_agr/api/local_routes/api.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, File, UploadFile
 from tags_agr import db_tools
 from tags_agr.api import service
+import os
 
 router = APIRouter()
 
 
 @router.get('/items-by-tag/{tag}')
-def get_items_by_tag(tag: str):
+def get_items_by_tag(tag: str, token: str):
     """Get items by tag."""
+    if token != os.environ.get('TOKEN'):
+        return {'error': 'Wrong token'}
     try:
         search_result = db_tools.get_items_by_tag(tag)
     except ValueError as val_err:
@@ -16,8 +19,10 @@ def get_items_by_tag(tag: str):
 
 
 @router.get('/items-by-title/{title}')
-def get_items_by_title(title: str):
+def get_items_by_title(title: str, token: str):
     """Get items by title."""
+    if token != os.environ.get('TOKEN'):
+        return {'error': 'Wrong token'}
     try:
         search_result = db_tools.get_items_by_title(title)
     except ValueError as val_err:
@@ -26,8 +31,10 @@ def get_items_by_title(title: str):
 
 
 @router.post('/items_xls')
-def push_items_xls(file_data: UploadFile = File(...)):
+def push_items_xls(token: str, file_data: UploadFile = File(...)):
     """Push item to db."""
+    if token != os.environ.get('TOKEN'):
+        return {'error': 'Wrong token'}
     try:
         service.push_xls_to_db(file_data)
     except ValueError as val_err:
--- tags_agr/db_tools.py
+++ tags_agr/db_tools.py
@@ -6,32 +6,30 @@ from tags_agr import schemas
 
 
 def push(raw_title: str, tags: List[str], batch_num: int):
-    db = SessionLocal()
-    title = raw_title.strip()
-    batch_item = db.query(models.Batch).filter_by(number=batch_num).first()
-    stock_item = db.query(models.StockItem).filter_by(
-        title=title,
-        batch=batch_item,
-    ).first()
-    if stock_item:
-        db.close()
-        return
+    with SessionLocal() as db:
+        title = raw_title.strip()
+        batch_item = db.query(models.Batch).filter_by(number=batch_num).first()
+        stock_item = db.query(models.StockItem).filter_by(
+            title=title,
+            batch=batch_item,
+        ).first()
+        if stock_item:
+            return
 
-    if not batch_item:
-        batch_item = models.Batch(number=batch_num)
-        db.add(batch_item)
-    stock_item = models.StockItem(title=title, batch=batch_item)
-    db.add(stock_item)
+        if not batch_item:
+            batch_item = models.Batch(number=batch_num)
+            db.add(batch_item)
+        stock_item = models.StockItem(title=title, batch=batch_item)
+        db.add(stock_item)
 
-    for raw_tag in set(tags):
-        tag = raw_tag.strip()
-        tag_item = db.query(models.Tag).filter_by(name=tag).first()
-        if not tag_item:
-            tag_item = models.Tag(name=tag)
-            db.add(tag_item)
-        stock_item.tags.append(tag_item)
-    db.commit()
-    db.close()
+        for raw_tag in set(tags):
+            tag = raw_tag.strip()
+            tag_item = db.query(models.Tag).filter_by(name=tag).first()
+            if not tag_item:
+                tag_item = models.Tag(name=tag)
+                db.add(tag_item)
+            stock_item.tags.append(tag_item)
+        db.commit()
 
 
 def get_items_by_title(title: str) -> schemas.SearchResult:
