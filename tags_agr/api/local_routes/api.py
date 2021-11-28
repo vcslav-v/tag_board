3e50db9b6c1f0ff18385713d99c20b176ed15965
Vcslav <vcslav.v@gmail.com>
2021-11-28 12:16:06 +0300

    refactoring auth

--- tags_agr/api/local_routes/api.py
+++ tags_agr/api/local_routes/api.py
@@ -1,16 +1,34 @@
-from fastapi import APIRouter, File, UploadFile
+import os
+import secrets
+
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from tags_agr import db_tools
 from tags_agr.api import service
-import os
 
 router = APIRouter()
+security = HTTPBasic()
+
+
+username = os.environ.get('API_USERNAME') or 'root'
+password = os.environ.get('API_PASSWORD') or 'pass'
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, username)
+    correct_password = secrets.compare_digest(credentials.password, password)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 @router.get('/items-by-tag/{tag}')
-def get_items_by_tag(tag: str, token: str):
+def get_items_by_tag(tag: str, _: str = Depends(get_current_username)):
     """Get items by tag."""
-    if token != os.environ.get('TOKEN'):
-        return {'error': 'Wrong token'}
     try:
         search_result = db_tools.get_items_by_tag(tag)
     except ValueError as val_err:
@@ -19,10 +37,8 @@ def get_items_by_tag(tag: str, token: str):
 
 
 @router.get('/items-by-title/{title}')
-def get_items_by_title(title: str, token: str):
+def get_items_by_title(title: str,  _: str = Depends(get_current_username)):
     """Get items by title."""
-    if token != os.environ.get('TOKEN'):
-        return {'error': 'Wrong token'}
     try:
         search_result = db_tools.get_items_by_title(title)
     except ValueError as val_err:
@@ -31,10 +47,8 @@ def get_items_by_title(title: str, token: str):
 
 
 @router.post('/items_xls')
-def push_items_xls(token: str, file_data: UploadFile = File(...)):
+def push_items_xls(_: str = Depends(get_current_username), file_data: UploadFile = File(...)):
     """Push item to db."""
-    if token != os.environ.get('TOKEN'):
-        return {'error': 'Wrong token'}
     try:
         service.push_xls_to_db(file_data)
     except ValueError as val_err:
